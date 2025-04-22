5affb63b5870dd9aa0e5d4f9d918d40ec228fef3
3x-haust <lyusungyun@icloud.com>
2025-04-22 22:23:16 +0900

    feat: enhance routing and theme configuration, add new OAuth pages

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,43 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import { LoginPage } from '../pages/login'
 import { NotFoundPage } from '../pages/notfound'
 import { OAuthCallback } from '../pages/oauth'
 import { HomePage } from '../pages/home'
+import { ConsentPage } from '../pages/oauth/consent'
+import { ManageOAuthAppsPage } from '../pages/oauth/manage'
+import { NewOAuthAppPage } from '../pages/oauth/new'
+import { EditOAuthAppPage } from '../pages/oauth/edit'
+import { useAuthStore } from '../features/auth'
+import { Header } from '../widgets/index'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles'
 
 function App() {
+  const { initializeAuth } = useAuthStore()
+  const location = useLocation()
+
+  useEffect(() => {
+    initializeAuth()
+  }, [initializeAuth])
+
+  const showHeader = location.pathname !== '/login'
+
   return (
     <>
-      <Routes>
-        <Route path='/oauth-login' element={<LoginPage />} />
-        <Route path='/' element={<HomePage />} />
-        <Route path='/oauth' element={<OAuthCallback />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+     <ThemeProvider theme={theme}>
+        {showHeader && <Header />}
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/' element={<HomePage />} />
+          <Route path='/oauth' element={<OAuthCallback />} />
+          <Route path='/oauth/consent' element={<ConsentPage />} />
+          <Route path='/oauth/manage' element={<ManageOAuthAppsPage />} />
+          <Route path='/oauth/new' element={<NewOAuthAppPage />} />
+          <Route path='/oauth/edit/:id' element={<EditOAuthAppPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+     </ThemeProvider>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,30 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync('./key.pem'),
-      cert: fs.readFileSync('./cert.pem'),
-    },
-    host: 'localhost',
-    port: 5173
-  },
-})
+export default defineConfig(({ mode }) => {
+  let config;
+
+  if (mode === 'development') {
+    config = {
+      plugins: [react()],
+      server: {
+        https: {
+          key: fs.readFileSync('./localhost-key.pem'),
+          cert: fs.readFileSync('./localhost.pem'),
+        },
+        host: 'localhost',
+        port: 5173
+      },
+    };
+  } else {
+    config = {
+      plugins: [react()],
+      server: {
+        host: 'localhost',
+        port: 5173
+      },
+    };
+  }
+
+  return config;
+});
