0d9d409e3ed51468c511e3c6d3f0a3bb720d7f5e
3x-haust <lyusungyun@icloud.com>
2026-05-19 22:20:36 +0900

    fix: docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  const isDevCommand = process.env.npm_lifecycle_event === 'dev';
-
-  const commonConfig = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-  };
-
-  if (isDevCommand) {
-    return {
-      ...commonConfig,
-      server: {
-        https: {
-          key: fs.readFileSync('./localhost-key.pem'),
-          cert: fs.readFileSync('./localhost.pem'),
-        },
-        host: 'localhost',
-        port: 5173,
-      },
-    };
-  } else {
-    return {
-      ...commonConfig,
-      server: {
-        host: 'localhost',
-        port: 5173,
-        allowedHosts: ['auth.mmhs.app'],
-      },
-    };
-  }
+  },
+  server: {
+    host: 'localhost',
+    port: 5173,
+    allowedHosts: ['auth.mmhs.app'],
+  },
 });
